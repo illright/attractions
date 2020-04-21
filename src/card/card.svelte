c8a6258daf619f34f6e941d48c1142d6b3ff700e
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-21 22:40:01 +0300

    Add Card component

---
+++ src/card/card.svelte
@@ -0,0 +1,28 @@
+<script>
+  import classes from '../_utils/classes.js';
+
+  let _class = null;
+  export { _class as class };
+  export let id = '';
+  export let imgSrc = '';
+  export let imgAlt = '';
+  export let title = '';
+  export let subtitle = '';
+
+  const withImage = !!imgSrc;
+</script>
+
+<div class={classes('card', _class, withImage && 'with-image')} {id}>
+  {#if withImage}
+    <img src={imgSrc} alt={imgAlt} class="image" />
+  {/if}
+  <div class="content">
+    {#if title || subtitle}
+      <p class="title">{title}</p>
+      <p class="subtitle">{subtitle}</p>
+    {/if}
+    <slot />
+  </div>
+</div>
+
+<style src="./card.scss"></style>
--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export { default as Button } from './button/button.svelte';
 
 export { default as RadioGroup } from './radio-button/radio-group.svelte';
 export { default as RadioButton } from './radio-button/radio-button.svelte';
+
+export { default as Card } from './card/card.svelte';
