6947db360876cf16049a7063bf782c9645248dbd
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-22 21:14:36 +0300

    Add config for demo page

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
+import autoPreprocess from 'svelte-preprocess';
 import pkg from './package.json';
 
 const name = pkg.name
@@ -7,14 +9,68 @@ const name = pkg.name
 	.replace(/^\w/, m => m.toUpperCase())
 	.replace(/-\w/g, m => m[1].toUpperCase());
 
-export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name },
-	],
-	plugins: [
-		svelte(),
-		resolve(),
-	],
-};
+const production = !process.env.ROLLUP_WATCH;
+
+export default [
+	{
+		input: 'src/index.js',
+		output: [
+			{ file: pkg.module, 'format': 'es' },
+			{ file: pkg.main, 'format': 'umd', name },
+		],
+		plugins: [
+			svelte(),
+			resolve(),
+		],
+	},
+	{
+		input: 'demo/main.js',
+		output: {
+			format: 'iife',
+			name: 'attractions',
+			file: 'demo/public/build/bundle.js',
+		},
+		plugins: [
+			svelte({
+				dev: !production,
+				hydratable: true,
+				preprocess: [
+					autoPreprocess({
+						scss: {
+							includePaths: [
+								'./demo/static/',
+							],
+						},
+					}),
+				],
+				css: css => css.write('demo/public/build/bundle.css'),
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte'],
+			}),
+			alias({
+				entries: [
+					{ find: /^@\//, replacement: __dirname + '/src/' },
+				],
+			}),
+			!production && serve(),
+		],
+	},
+];
+
+function serve() {
+	let started = false;
+
+	return {
+		writeBundle() {
+			if (started) return;
+			started = true;
+
+			require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true,
+			});
+		},
+	};
+}
