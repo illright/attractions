5e54ed287e8948696503fa9d888ac00409d4dbe1
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-04-22 22:31:17 +0300

    Replace tabs with spaces

--- rollup.config.js
+++ rollup.config.js
@@ -4,67 +4,67 @@ import autoPreprocess from 'svelte-preprocess';
 import pkg from './package.json';
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default [
-	{
-		input: 'src/index.js',
-		output: [
-			{ file: pkg.module, 'format': 'es' },
-			{ file: pkg.main, 'format': 'umd', name },
-		],
-		plugins: [
-			svelte(),
-			resolve(),
-		],
-	},
-	{
-		input: 'demo/main.js',
-		output: {
-			format: 'iife',
-			name: 'attractions',
-			file: 'demo/public/build/bundle.js',
-		},
-		plugins: [
-			svelte({
-				dev: !production,
-				hydratable: true,
-				preprocess: [
-					autoPreprocess({
-						scss: {
-							includePaths: [
-								'./demo/static/',
-							],
-						},
-					}),
-				],
-				css: css => css.write('demo/public/build/bundle.css'),
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte'],
-			}),
-			!production && serve(),
-		],
-	},
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.module, 'format': 'es' },
+      { file: pkg.main, 'format': 'umd', name },
+    ],
+    plugins: [
+      svelte(),
+      resolve(),
+    ],
+  },
+  {
+    input: 'demo/main.js',
+    output: {
+      format: 'iife',
+      name: 'attractions',
+      file: 'demo/public/build/bundle.js',
+    },
+    plugins: [
+      svelte({
+        dev: !production,
+        hydratable: true,
+        preprocess: [
+          autoPreprocess({
+            scss: {
+              includePaths: [
+                './demo/static/',
+              ],
+            },
+          }),
+        ],
+        css: css => css.write('demo/public/build/bundle.css'),
+      }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte'],
+      }),
+      !production && serve(),
+    ],
+  },
 ];
 
 function serve() {
-	let started = false;
+  let started = false;
 
-	return {
-		writeBundle() {
-			if (started) return;
-			started = true;
+  return {
+    writeBundle() {
+      if (started) return;
+      started = true;
 
-			require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true,
-			});
-		},
-	};
+      require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true,
+      });
+    },
+  };
 }
