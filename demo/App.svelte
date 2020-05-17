064b0595b792cccd406f9b6524d04429bd165491
illright <leva181777@gmail.com>
2020-05-18 00:47:08 +0300

    Create a demo for modals

--- demo/App.svelte
+++ demo/App.svelte
@@ -10,7 +10,10 @@
     Switch,
     RadioChipGroup,
     CheckboxChipGroup,
+    ModalOverlay,
+    Modal,
   } from '../src/index.js';
+  import ModalCard from './modal-card.svelte';
   import { ChevronDownIcon } from 'svelte-feather-icons';
 
   let items = [
@@ -29,50 +32,61 @@
   ];
   let item = items[1].value;
   let colorItem = colorItems[1].value;
+
+  let open1 = false;
+  let open2 = false;
 </script>
 
-<Card>
-  <TextField label="Write something" outline />
-  <Button filled>
-    Button!!
-  </Button>
-  <RadioGroup items={colorItems} color bind:value={colorItem} name="colors" />
-  <RadioGroup items={items} bind:value={item} name="numbers" />
-  <CheckboxGroup items={items} max={1} name="numbers-check" />
-  <div class="dropdown-holder">
-    <DropdownShell let:toggle on:change={() => console.log('yay')}>
-      <Button on:click={toggle}>
-        test me!
-        <ChevronDownIcon size="24" class="ml dropdown-chevron" />
-      </Button>
-      <Dropdown top>
-        <div class="padded">
-          I'm a little dropdown short and stout
-        </div>
-      </Dropdown>
-    </DropdownShell>
-  </div>
+<ModalOverlay>
+  <Card>
+    <TextField label="Write something" outline />
+    <Button filled>
+      Button!!
+    </Button>
+    <RadioGroup items={colorItems} color bind:value={colorItem} name="colors" />
+    <RadioGroup items={items} bind:value={item} name="numbers" />
+    <CheckboxGroup items={items} max={1} name="numbers-check" />
+    <div class="dropdown-holder">
+      <DropdownShell let:toggle on:change={() => console.log('yay')}>
+        <Button on:click={toggle}>
+          test me!
+          <ChevronDownIcon size="24" class="ml dropdown-chevron" />
+        </Button>
+        <Dropdown top>
+          <div class="padded">
+            I'm a little dropdown short and stout
+          </div>
+        </Dropdown>
+      </DropdownShell>
+    </div>
 
-  <Switch>
-    <span class="padded">
-      default
-    </span>
-  </Switch>
-  <Switch slotLeft value={true}>
-    <span class="padded">
-      on
-    </span>
-  </Switch>
-  <Switch slotLeft value={true} disabled>
-    <span class="padded">
-      disabled
-    </span>
-  </Switch>
-  <div class="flex">
-    <RadioChipGroup {items} name="radio-chip-group" outline />
-    <CheckboxChipGroup {items} name="checkbox-chip-group" max={2} small />
-  </div>
-</Card>
+    <Switch>
+      <span class="padded">
+        default
+      </span>
+    </Switch>
+    <Switch slotLeft value={true}>
+      <span class="padded">
+        on
+      </span>
+    </Switch>
+    <Switch slotLeft value={true} disabled>
+      <span class="padded">
+        disabled
+      </span>
+    </Switch>
+    <div class="flex">
+      <RadioChipGroup {items} name="radio-chip-group" outline />
+      <CheckboxChipGroup {items} name="checkbox-chip-group" max={2} small />
+    </div>
+    <div class="flex">
+      <Button on:click={() => open1 = true}>open modal 1</Button>
+      <Button on:click={() => open2 = true}>open modal 2</Button>
+    </div>
+    <Modal component={ModalCard} props={{ doYou: 'hear what I hear' }} bind:open={open1} />
+    <Modal component={ModalCard} props={{ doYou: 'care if I care' }} bind:open={open2} />
+  </Card>
+</ModalOverlay>
 
 <style>
   .padded {
