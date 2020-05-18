00cae44d3ba022ccff3fd4fb4a4524680096b691
illright <leva181777@gmail.com>
2020-05-18 10:11:25 +0300

    Implement the Dot component and its demo

--- src/index.js
+++ src/index.js
@@ -20,3 +20,5 @@ export { default as RadioChip } from './chip/radio-chip.svelte';
 export { default as RadioChipGroup } from './chip/radio-chip-group.svelte';
 export { default as CheckboxChip } from './chip/checkbox-chip.svelte';
 export { default as CheckboxChipGroup } from './chip/checkbox-chip-group.svelte';
+
+export { default as Dot } from './dot/dot.svelte';
