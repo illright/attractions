763e650f74e4a13eb8802f3b1c1c5cd51aa8cfb3
illright <leva181777@gmail.com>
2020-05-24 14:13:07 +0300

    Clear a useless wrapper div

--- src/date-picker/calendar.svelte
+++ src/date-picker/calendar.svelte
@@ -22,48 +22,46 @@
   const dispatch = createEventDispatcher();
 </script>
 
-<div>
-  <div class={classes('weekdays', weekdaysClass)}>
-    {#each weekdays as dayName (dayName)}
-      <span class="weekday">{dayName}</span>
-    {/each}
-  </div>
-  {#each getCalendar(month, year, firstWeekday) as week}
-    <div class={classes('week', weekClass)}>
-      <!--
-        The following .day elements may have one of the classes:
-        * .in-range: day is in range for range pickers
-        * .selected: day is selected or is a range boundary
-        * .outside:  day is not in this month
-        * .today:    day is today
-        * .start:    day is the beginning of a range
-        * .end:      day is the end of a range
-      -->
-      {#each week as day}
-        <div
-          class={classes('day', dayClass)}
-          class:today={datesEqual(day.value, today)}
-          class:outside={day.outside}
-          class:selected={
-            datesEqual(day.value, selectionStart) || datesEqual(day.value, selectionEnd)
-          }
-          class:start={datesEqual(day.value, selectionStart)}
-          class:end={datesEqual(day.value, selectionEnd)}
-          class:in-range={
-            datesLessEqual(selectionStart, day.value)
-            && datesLessEqual(day.value, selectionEnd)
-          }
-        >
-          <Button
-            title={datesEqual(day.value, today) && 'Today' || null}
-            on:click={(e) => { e.stopPropagation(); dispatch('day-select', day.value); }}
-          >
-            {dayNumberFormatter.format(day.value)}
-          </Button>
-        </div>
-      {/each}
-    </div>
+<div class={classes('weekdays', weekdaysClass)}>
+  {#each weekdays as dayName (dayName)}
+    <span class="weekday">{dayName}</span>
   {/each}
 </div>
+{#each getCalendar(month, year, firstWeekday) as week}
+  <div class={classes('week', weekClass)}>
+    <!--
+      The following .day elements may have one of the classes:
+      * .in-range: day is in range for range pickers
+      * .selected: day is selected or is a range boundary
+      * .outside:  day is not in this month
+      * .today:    day is today
+      * .start:    day is the beginning of a range
+      * .end:      day is the end of a range
+    -->
+    {#each week as day}
+      <div
+        class={classes('day', dayClass)}
+        class:today={datesEqual(day.value, today)}
+        class:outside={day.outside}
+        class:selected={
+          datesEqual(day.value, selectionStart) || datesEqual(day.value, selectionEnd)
+        }
+        class:start={datesEqual(day.value, selectionStart)}
+        class:end={datesEqual(day.value, selectionEnd)}
+        class:in-range={
+          datesLessEqual(selectionStart, day.value)
+          && datesLessEqual(day.value, selectionEnd)
+        }
+      >
+        <Button
+          title={datesEqual(day.value, today) && 'Today' || null}
+          on:click={(e) => { e.stopPropagation(); dispatch('day-select', day.value); }}
+        >
+          {dayNumberFormatter.format(day.value)}
+        </Button>
+      </div>
+    {/each}
+  </div>
+{/each}
 
 <style src="./calendar.scss"></style>
