6173307a68a86d72b6b48b4689de8ab9d98a0ed1
illright <leva181777@gmail.com>
2020-05-27 09:07:25 +0300

    Simplify the Modal implementation

--- src/modal/modal.svelte
+++ src/modal/modal.svelte
@@ -1,44 +1,21 @@
 <script>
-  import { getContext, onMount, onDestroy } from 'svelte';
-  import modalContextKey from './modal-context-key.js';
+  import classes from '../_utils/classes.js';
 
-  const { register, update, unregister } = getContext(modalContextKey);
-
+  let _class = null;
+  export { _class as class };
   export let open = false;
 
-  /* The component to use in creating a modal. */
-  export let component;
-
-  /* The props to pass to the modal component.
-     It will also receive the `closeCallback` prop which can be used to close the modal. */
-  export let props = {};
-
-  let openState = null;
-  let unsubscribe = null;
-
-  $: syncOpenState(open);
-  $: update(openState, props);
-
-  /* Written out as a separate function to ensure
-     that the reactive statement only depends on `open`. */
-  function syncOpenState(openStateValue) {
-    openState && openState.set(openStateValue);
+  function close() {
+    open = false;
   }
-
-  onMount(() => {
-    openState = register(component, props);
-    unsubscribe = openState.subscribe((openStateValue) => {
-      /* To prevent unneeded invalidation (if Svelte isn't smart enough not to) */
-      if (open !== openStateValue) {
-        open = openStateValue;
-      }
-    });
-  });
-
-  onDestroy(() => {
-    unregister(openState);
-    if (unsubscribe != null) {
-      unsubscribe();
-    }
-  });
 </script>
+
+<div
+  class:open
+  on:click|self={close}
+  class={classes('modal-overlay', _class)}
+>
+  <slot closeCallback={close} />
+</div>
+
+<style src="./modal-overlay.scss"></style>
