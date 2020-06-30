afa2c13e768a292dc2dd381a6cfbe6bcf6af93bf
illright <leva181777@gmail.com>
2020-06-30 11:45:43 +0300

    Set up ESLint and Stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true,
+  },
+  "extends": ["eslint:recommended"],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 2019,
+    "sourceType": "module",
+    "allowImportExportEverywhere": false,
+  },
+  "plugins": ["svelte3"],
+  "overrides": [
+    {
+      "files": ["**/*.svelte"],
+      "processor": "svelte3/svelte3",
+    },
+  ],
+  "globals": {
+    "__DEBUG__": false,
+    "__DEV__": false,
+    "__HOT__": false,
+  },
+  "settings": {
+    "svelte3/ignore-styles": (attrs) => attrs.lang === 'scss',
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "spaced-comment": ["error", "always"],
+    "space-before-function-paren": ["error", {
+      "anonymous": "never",
+      "named": "never",
+      "asyncArrow": "always",
+    }],
+    "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+    "no-trailing-spaces": ["error", {
+      "skipBlankLines": false,
+      "ignoreComments": false,
+    }],
+    "curly": ["warn", "all"],
+    "indent": ["error", 2],
+    "comma-dangle": ["warn", "always-multiline"],
+    "no-unused-vars": [
+      "error",
+      { "vars": "all", "args": "none", "ignoreRestSiblings": true },
+    ],
+    "no-constant-condition": "off",
+    "keyword-spacing": "error",
+    "no-console": ["warn", { "allow": ["warn", "error"] }],
+    "no-template-curly-in-string": "warn",
+    "block-spacing": ["error", "always"],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "computed-property-spacing": ["error", "never"],
+    "eol-last": ["error", "always"],
+    "max-len": ["error", { "code": 100 }],
+    "linebreak-style": ["error", "unix"],
+    "no-tabs": "error",
+    "operator-assignment": ["error", "always"],
+    "object-curly-spacing": ["warn", "always", {
+      "arraysInObjects": true,
+      "objectsInObjects": true,
+    }],
+    "key-spacing": "error",
+    "space-in-parens": ["error", "never"],
+    "space-infix-ops": "error",
+    "no-var": "error",
+    "prefer-const": ["error", {
+      "destructuring": "all",
+      "ignoreReadBeforeAssign": false,
+    }],
+    "yield-star-spacing": "error",
+    "generator-star-spacing": ["error", {
+      "before": false,
+      "after": true,
+      "anonymous": "neither",
+      "method": { "before": true, "after": false },
+    }],
+    "camelcase": "warn",
+  },
+};
