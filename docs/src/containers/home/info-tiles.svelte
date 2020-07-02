d584ca7dc2e060bfcdcf79b34643433065bf3f01
illright <leva181777@gmail.com>
2020-07-02 19:16:28 +0300

    Build the home page

---
+++ docs/src/containers/home/info-tiles.svelte
@@ -0,0 +1,28 @@
+<script>
+  import * as attractions from 'attractions';
+  import { s } from 'attractions/utils';
+  import { GridIcon, Edit2Icon, FeatherIcon } from 'svelte-feather-icons';
+  import InfoTile from 'src/components/home/info-tile.svelte';
+
+  const totalComponents = [...Object.keys(attractions)].length;
+</script>
+
+<div class="info-tiles">
+  <InfoTile
+    icon={GridIcon}
+    title="{totalComponents} component{s(totalComponents)}"
+    subtitle="and more to come!"
+  />
+  <InfoTile
+    icon={Edit2Icon}
+    title="Stylable with Sass"
+    subtitle="customize colors, fonts, shadows!"
+  />
+  <InfoTile
+    icon={FeatherIcon}
+    title="How to compute bundle size?"
+    subtitle="please help me!"
+  />
+</div>
+
+<style src="../../../static/css/containers/home/info-tiles.scss"></style>
---
+++ docs/src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { Card, Button, H1 } from 'attractions';
+  import { BookOpenIcon, GithubIcon } from 'svelte-feather-icons';
+  import InfoTiles from 'src/containers/home/info-tiles.svelte';
+</script>
+
+<main class="padded">
+  <Card>
+    <header>
+      <img src="/logo-no-bg.svg" alt="Attractions logo" />
+      <H1>Attractions</H1>
+      <a href="/changelog" class="hide-on-tb-more">v{process.latest_version}</a>
+    </header>
+    <p>A <mark>pretty cool</mark> UI kit for Svelte.</p>
+    <div class="actions">
+      <Button filled href="/docs">
+        <BookOpenIcon size="24" class="mr" />
+        docs
+      </Button>
+      <Button outline class="ml" href="https://github.com/illright/attractions">
+        <GithubIcon size="24" class="mr" />
+        GitHub
+      </Button>
+    </div>
+  </Card>
+  <div class="strip" />
+  <InfoTiles />
+  <footer>
+    <p>
+      made with ❤ by
+      <a href="https://github.com/aabounegm">@aabounegm</a>
+      and
+      <a href="https://github.com/illright">@illright</a>
+    </p>
+    <p>
+      {process.license} licensed
+      &nbsp;•&nbsp;
+      <a href="/changelog">v{process.latest_version}</a>
+    </p>
+  </footer>
+</main>
+
+<style src="../../static/css/routes/index.scss"></style>
