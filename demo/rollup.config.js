209dd56a12deb9d908ec387a7b3d5040487bb333
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-03 21:09:02 +0300

    Fix up paths in the demo config

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -5,40 +5,38 @@ import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default [
-  {
-    input: 'demo/main.js',
-    output: {
-      format: 'iife',
-      name: 'attractions',
-      file: 'demo/public/build/bundle.js',
-    },
-    plugins: [
-      svelte({
-        dev: !production,
-        hydratable: true,
-        preprocess: [
-          autoPreprocess({
-            scss: {
-              includePaths: [
-                './demo/static/',
-              ],
-            },
-          }),
-        ],
-        css: css => css.write('demo/public/build/bundle.css', false),
-      }),
-      resolve({
-        browser: true,
-        dedupe: ['svelte'],
-      }),
-      !production && serve(),
-      production && terser({
-        module: true,
-      }),
-    ],
+export default {
+  input: 'main.js',
+  output: {
+    format: 'iife',
+    name: 'attractions',
+    file: 'public/build/bundle.js',
   },
-];
+  plugins: [
+    svelte({
+      dev: !production,
+      hydratable: true,
+      preprocess: [
+        autoPreprocess({
+          scss: {
+            includePaths: [
+              './static/',
+            ],
+          },
+        }),
+      ],
+      css: css => css.write('public/build/bundle.css', false),
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    !production && serve(),
+    production && terser({
+      module: true,
+    }),
+  ],
+};
 
 function serve() {
   let started = false;
