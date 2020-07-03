fc180505bb2d04ba20582cb01d49097ac8a8e932
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-07-03 20:53:32 +0300

    Separate attractions rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,10 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import autoPreprocess from 'svelte-preprocess';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './attractions/package.json';
-
-const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase());
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default [
-  {
-    input: 'attractions/index.js',
-    output: [
-      { file: `attractions/${pkg.module}`, 'format': 'es' },
-      { file: `attractions/${pkg.main}`, 'format': 'umd', name },
-    ],
-    plugins: [
-      svelte(),
-      resolve(),
-    ],
-  },
   {
     input: 'demo/main.js',
     output: {
