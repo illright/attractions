ccf153d5fba8dac94289c4e1ee00c0fbe4c77a7e
Lev Chelyadinov <leva181777@gmail.com>
2020-07-09 22:07:37 +0300

    Implement the header and the beginnings of the navigation

---
+++ docs/src/containers/docs/header.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Button } from 'attractions';
+  import { GithubIcon } from 'svelte-feather-icons';
+
+  const logoSize = 30;
+</script>
+
+<header class="padded">
+  <a href="/" class="logo">
+    <img src="logo-no-bg.svg" width={logoSize} height={logoSize} alt="Logo" />
+    <span class="hide-on-less-tb">Attractions</span>
+  </a>
+  <div class="clickables">
+    <!-- TODO: add the versions here
+    <DropdownShell let:toggle>
+      <Button small on:click={toggle}>
+        v{process.latest_version}
+        <ChevronDownIcon size="20" class="dropdown-chevron ml" />
+      </Button>
+      <Dropdown>
+
+      </Dropdown>
+    </DropdownShell> -->
+    <Button round href="https://github.com/illright/attractions">
+      <GithubIcon size="24" />
+    </Button>
+  </div>
+</header>
+
+<style src="../../../static/css/containers/docs/header.scss"></style>
