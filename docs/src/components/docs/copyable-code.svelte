a52de68f1ef97ec2f479013e0befe2e198f3557a
Lev Chelyadinov <leva181777@gmail.com>
2020-07-12 22:11:23 +0300

    Implement the code-copying `<pre>` wrapper

---
+++ docs/src/components/docs/copyable-code.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from 'svelte';
+  import { Button } from 'attractions';
+  import { CopyIcon, CheckIcon } from 'svelte-feather-icons';
+  import { writeText } from 'clipboard-polyfill';
+
+  let self;
+  let text = null;
+  let copied = false;
+
+  onMount(function extractCodeToCopy() {
+    text = self.children[0].textContent;
+  });
+
+  async function copy() {
+    if (text == null) {
+      return;
+    }
+
+    try {
+      await writeText(text);
+      copied = true;
+      setTimeout(() => copied = false, 1000);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+</script>
+
+<div class="copyable" bind:this={self}>
+  <slot />
+  <Button small round title="Copy" on:click={copy}>
+    {#if copied}
+      <CheckIcon size="20" />
+    {:else}
+      <CopyIcon size="20" />
+    {/if}
+  </Button>
+</div>
+
+<style src="../../../static/css/components/docs/copyable-code.scss"></style>
--- docs/src/containers/docs/showcase.svelte
+++ docs/src/containers/docs/showcase.svelte
@@ -1,12 +1,15 @@
 <script>
   import { Label } from 'attractions';
+  import CopyableCode from 'src/components/docs/copyable-code.svelte';
 </script>
 
 <section class="showcase">
   <Label>Showcase</Label>
   <slot name="showcase" />
   <Label class="code">Source</Label>
-  <slot name="source" />
+  <CopyableCode>
+    <slot name="source" />
+  </CopyableCode>
 </section>
 
 <style src="../../../static/css/containers/docs/showcase.scss"></style>
