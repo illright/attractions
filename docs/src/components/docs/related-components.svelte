db34d49de5b5a8355c397251254e9e6b28ef3d29
Lev Chelyadinov <leva181777@gmail.com>
2020-07-13 11:30:13 +0300

    Document the <Accordion> component

---
+++ docs/src/components/docs/related-components.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Label } from 'attractions';
+
+  export let components;
+</script>
+
+{#if components.length !== 0}
+  <ul class="related-components">
+    <Label>Related Components</Label>
+    {#each components as component}
+      <li>
+        <a href={component.link}><code>{component.name}</code></a>
+      </li>
+    {/each}
+  </ul>
+{/if}
+
+<style src="../../../static/css/components/docs/related-components.scss"></style>
