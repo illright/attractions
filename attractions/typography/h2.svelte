f93380b201f39c10a52a3ffabee937b4ca9d8fe2
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-08-21 11:56:00 +0300

    Add consistent behavior to typography elements

--- attractions/typography/h2.svelte
+++ attractions/typography/h2.svelte
@@ -1,3 +1,10 @@
-<h2 {...$$restProps}><slot /></h2>
+<script>
+  import classes from '../utils/classes.js';
+
+  let _class = null;
+  export { _class as class };
+</script>
+
+<h2 class={classes(_class)} {...$$restProps}><slot /></h2>
 
 <style src="./h2.scss"></style>
--- attractions/typography/h3.svelte
+++ attractions/typography/h3.svelte
@@ -1,3 +1,10 @@
-<h3 {...$$restProps}><slot /></h3>
+<script>
+  import classes from '../utils/classes.js';
+
+  let _class = null;
+  export { _class as class };
+</script>
+
+<h3 class={classes(_class)} {...$$restProps}><slot /></h3>
 
 <style src="./h3.scss"></style>
