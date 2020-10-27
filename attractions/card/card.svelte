9fe91c12c2d33565709f2316e06dfa5847a362d9
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-10-27 22:34:23 +0300

    Add example for demo with a couple of components

--- attractions/card/card.svelte
+++ attractions/card/card.svelte
@@ -1,3 +1,5 @@
+<svelte:options tag="a-card" />
+
 <script>
   import classes from '../utils/classes.js';
 
--- attractions/switch/switch.svelte
+++ attractions/switch/switch.svelte
@@ -1,3 +1,5 @@
+<svelte:options tag="a-switch" />
+
 <script>
   import { createEventDispatcher } from 'svelte';
   import classes from '../utils/classes.js';
