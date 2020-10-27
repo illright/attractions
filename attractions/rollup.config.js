4b2dea6c6c4eb9b7ceed5026c9e8f72f7a895e91
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2020-10-27 22:29:49 +0300

    Add option to compile as customElements

--- attractions/rollup.config.js
+++ attractions/rollup.config.js
@@ -1,5 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
+import autoPreprocess from 'svelte-preprocess';
 import pkg from './package.json';
 
 const name = pkg.name
@@ -7,14 +8,34 @@ const name = pkg.name
   .replace(/^\w/, m => m.toUpperCase())
   .replace(/-\w/g, m => m[1].toUpperCase());
 
-export default {
-  input: 'index.js',
-  output: [
-    { file: pkg.module, 'format': 'es' },
-    { file: pkg.main, 'format': 'umd', name },
-  ],
-  plugins: [
-    svelte(),
-    resolve(),
-  ],
-};
+export default [
+  {
+    input: 'index.js',
+    output: [
+      { file: pkg.module, 'format': 'es' },
+      { file: pkg.main, 'format': 'umd', name },
+    ],
+    plugins: [
+      svelte(),
+      resolve(),
+    ],
+  },
+  {
+    // This will likely replace the other configuration above
+    input: 'index.js',
+    output: {
+      file: 'dist/bundle.js',
+      format: 'iife',
+      name: 'attractions',
+    },
+    plugins: [
+      svelte({
+        customElement: true,
+        preprocess: autoPreprocess({
+          scss: { includePaths: ['./'] },
+        }),
+      }),
+      resolve(),
+    ],
+  },
+];
