bb300f33b1d1544f3ccd66a90c961f9f8a2c096e
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2020-11-21 21:52:45 +0300

    Revert "Add example for demo with a couple of components"
    This reverts commit 9fe91c12c2d33565709f2316e06dfa5847a362d9.

--- attractions/card/card.svelte
+++ attractions/card/card.svelte
@@ -1,5 +1,3 @@
-<svelte:options tag="a-card" />
-
 <script>
   import classes from '../utils/classes.js';
 
--- attractions/switch/switch.svelte
+++ attractions/switch/switch.svelte
@@ -1,5 +1,3 @@
-<svelte:options tag="a-switch" />
-
 <script>
   import { createEventDispatcher } from 'svelte';
   import classes from '../utils/classes.js';
