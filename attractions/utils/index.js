1af6834696d831773e7148240bd83627d556431a
Lev Chelyadinov <leva181777@gmail.com>
2020-12-20 14:55:09 +0300

    Export and document the `escapeRegExp` utility

--- attractions/utils/index.js
+++ attractions/utils/index.js
@@ -2,6 +2,7 @@ export { default as callOnSight } from './call-on-sight.js';
 export { default as classes } from './classes.js';
 export { default as getColorPickerStyles } from './color-picker-styles.js';
 export { default as dynamicTransition } from './dynamic-transition.js';
+export { default as escapeRegExp } from './escape-regexp.js';
 export { default as events } from './events.js';
 export { default as formatFileSize } from './format-file-size.js';
 export { default as formatFileType } from './format-file-type.js';
