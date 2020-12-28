cea1e4eb668c1aa54fb7b6d61a3472e8506e319b
Lev Chelyadinov <leva181777@gmail.com>
2020-12-28 13:15:06 +0300

    Fix the formatting and add Prettier to the lint hook

--- docs/src/components/docs/shadow-preview.svelte
+++ docs/src/components/docs/shadow-preview.svelte
@@ -12,9 +12,7 @@
 </script>
 
 <Popover position={PopoverPositions.LEFT}>
-  <div class="preview" bind:this={preview}>
-    hover me
-  </div>
+  <div class="preview" bind:this={preview}>hover me</div>
   <div slot="popover-content" class="value-popover">
     {value.replace(/\), /g, '),\n')}
   </div>
