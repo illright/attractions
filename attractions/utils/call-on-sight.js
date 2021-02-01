d49e50085fe87ed1dd27bbfa6f48275d000e459c
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-02-01 19:29:45 +0300

    Change `any` type to a generic parameter

--- attractions/utils/call-on-sight.js
+++ attractions/utils/call-on-sight.js
@@ -2,8 +2,9 @@
  * Action that allows calling a function when an element appears in the viewport.
  * Uses the IntersectionObserver API, which might not be available in older browsers like IE11.
  * If it is not available, the action will be silently ignored.
+ * @template T
  * @param {Element} node The HTML element to be observed
- * @param {{ callback: (...args: any[]) => void; args: any[] }} options Callback and its args
+ * @param {{ callback: (...args: T[]) => void; args: T[] }} options Callback and its args
  * @returns {{ destroy: () => void }}
  */
 export default function callOnSight(node, { callback, args = [] }) {
