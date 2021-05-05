40b7651175f87a673fe1d393452f18a52fd2f0b2
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-05-05 16:47:54 +0200

    Replace the `id` function with explicit type

--- attractions/snackbar/snackbar-positions.js
+++ attractions/snackbar/snackbar-positions.js
@@ -1,21 +1,25 @@
 /**
- * Identity function. Coerces string/number literals to literal types.
  * This is needed to improve type inference in JSDoc, as it doesn't yet support `as const` assertions.
  * See [microsoft/TypeScript#30445](https://github.com/microsoft/TypeScript/issues/30445) for more details.
- * @template {string | number} T
- * @param {T} v
- * @return {T}
+ * @type {{
+ *  TOP_LEFT: 'top left',
+ *  TOP_MIDDLE: 'top h-middle',
+ *  TOP_RIGHT: 'top right',
+ *  MIDDLE_RIGHT: 'v-middle right',
+ *  BOTTOM_RIGHT: 'bottom right',
+ *  BOTTOM_MIDDLE: 'bottom h-middle',
+ *  BOTTOM_LEFT: 'bottom left',
+ *  MIDDLE_LEFT: 'v-middle left',
+ * }}
  */
-const id = v => v;
-
-export default Object.freeze({
-  // The types of all values would be detected as `string` if not using the `id` function
-  TOP_LEFT: id('top left'),
-  TOP_MIDDLE: id('top h-middle'),
-  TOP_RIGHT: id('top right'),
-  MIDDLE_RIGHT: id('v-middle right'),
-  BOTTOM_RIGHT: id('bottom right'),
-  BOTTOM_MIDDLE: id('bottom h-middle'),
-  BOTTOM_LEFT: id('bottom left'),
-  MIDDLE_LEFT: id('v-middle left'),
-});
+const positions = {
+  TOP_LEFT: 'top left',
+  TOP_MIDDLE: 'top h-middle',
+  TOP_RIGHT: 'top right',
+  MIDDLE_RIGHT: 'v-middle right',
+  BOTTOM_RIGHT: 'bottom right',
+  BOTTOM_MIDDLE: 'bottom h-middle',
+  BOTTOM_LEFT: 'bottom left',
+  MIDDLE_LEFT: 'v-middle left',
+};
+export default Object.freeze(positions);
