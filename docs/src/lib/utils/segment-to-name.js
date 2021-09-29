c335b825e883d9e14505c0996cc6761ce3f53f77
Lev Chelyadinov <leva181777@gmail.com>
2021-09-29 23:50:09 +0300

    Handle empty words in the `capitalize` function

--- docs/src/lib/utils/segment-to-name.js
+++ docs/src/lib/utils/segment-to-name.js
@@ -1,5 +1,9 @@
 // Return the word with only the first letter uppercased
 function capitalize(word) {
+  if (word.length < 1) {
+    return word;
+  }
+
   return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
