6f23f92207891c9621e2ccc98c7d69fedc98da14
Th1nkK1D <witheep@gmail.com>
2021-10-28 20:51:03 +0300

    Add new dropdown alignment props

--- attractions/dropdown/dropdown.svelte
+++ attractions/dropdown/dropdown.svelte
@@ -1,3 +1,22 @@
+<script context="module">
+  export const HorizontalAlignment = {
+    autoStart: 'auto-start',
+    autoEnd: 'auto-end',
+    start: 'start',
+    end: 'end',
+  };
+
+  export const VerticalAlignment = {
+    autoBottom: 'auto-bottom',
+    autoTop: 'auto-top',
+    bottom: 'bottom',
+    top: 'top',
+  };
+
+  Object.freeze(HorizontalAlignment);
+  Object.freeze(VerticalAlignment);
+</script>
+
 <script>
   import classes from '../utils/classes.js';
 
@@ -13,9 +32,23 @@
    * @type {boolean}
    */
   export let top = false;
+  /**
+   * Dropdown horizontal alignment relative to the shell.
+   * @type {string}
+   */
+  export let horizontalAlignment = HorizontalAlignment.autoStart;
+  /**
+   * Dropdown vertical alignment relative to the shell.
+   * @type {string}
+   */
+  export let verticalAlignment = VerticalAlignment.autoBottom;
 </script>
 
-<div class:right class:top class={classes('dropdown', _class)}>
+<div
+  class:right={right || horizontalAlignment === HorizontalAlignment.end}
+  class:top={top || verticalAlignment === VerticalAlignment.top}
+  class={classes('dropdown', _class)}
+>
   <slot />
 </div>
 
