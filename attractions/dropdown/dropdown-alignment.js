c45ed86833ceaefa24abbf428dfe9d57976f20f4
Th1nkK1D <witheep@gmail.com>
2021-12-22 22:54:03 +0200

    Fix dropdown jsdoc indent

--- attractions/dropdown/dropdown-alignment.js
+++ attractions/dropdown/dropdown-alignment.js
@@ -19,10 +19,10 @@ export const HorizontalAlignment = {
  * This is needed to improve type inference in JSDoc, as it doesn't yet support `as const` assertions.
  * See [microsoft/TypeScript#30445](https://github.com/microsoft/TypeScript/issues/30445) for more details.
  * @type {{
- * AUTO_BOTTOM: 'auto-bottom',
- * AUTO_TOP: 'auto-top',
- * BOTTOM: 'bottom',
- * TOP: 'top',
+ *  AUTO_BOTTOM: 'auto-bottom',
+ *  AUTO_TOP: 'auto-top',
+ *  BOTTOM: 'bottom',
+ *  TOP: 'top',
  * }}
  */
 export const VerticalAlignment = {
