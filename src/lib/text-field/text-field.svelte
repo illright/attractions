e3081d99df71c72cb57bcec8951c322b8ef94235
Lev Chelyadinov <leva181777@gmail.com>
2023-01-15 01:54:21 +0100

    Start redesigning the text field
    Co-authored-by: Abdelrahman Aly Abounegm <aabounegm@gmail.com>

--- src/lib/text-field/text-field.svelte
+++ src/lib/text-field/text-field.svelte
@@ -8,43 +8,28 @@
 
   let _class: string | false | null = null;
   export { _class as class };
+
   /**
    * A class string to assign to the `<input>` or `<textarea>` element.
    */
   export let inputClass: string | false | null = null;
   /**
-   * A class string to assign to the `<label>` for the outline text fields.
-   */
-  export let labelClass: string | false | null = null;
-  /**
    * A class string to add to the error message under the text field.
    */
   export let errorClass: string | false | null = null;
 
+  type TextFieldVariant = 'outlined' | 'underlined';
   /**
-   * Whether the text field should have outline styling.
+   *
    */
-  export let outline = false;
-  /**
-   * Whether there will be something permanent inside the field like an icon.
-   * By default, this item is placed to the left of the input area.
-   */
-  export let withItem = false;
-  /**
-   * Whether the item should be placed to the right of the input area.
-   */
-  export let itemRight = false;
+  export let variant: TextFieldVariant = 'underlined';
 
   /**
    * The ID to assign to the input.
    */
-  export let id: string | null = null;
+  export let id: string;
   /**
-   * Whether to hide the spinner (arrow buttons inside `<input type="number">`).
-   */
-  export let noSpinner = false;
-  /**
-   * The label to show above the text field. Only works with `outline` text fields.
+   * The label to show above the text field.
    */
   export let label: string | null = null;
   /**
@@ -60,108 +45,158 @@
    */
   export let autofocus = false;
 
-  if (!outline && label != null) {
-    console.error('Labels are only available for outlined text fields');
-  }
-
   /**
    * The current value of the text field. Converted to a number if `type="number"`.
    */
-  export let value: string | number = '';
+  export let value = '';
   /**
    * List of handlers for the [Events](https://illright.github.io/attractions/docs/utilities) action.
    */
   export let events: Array<{ name: string; handler: (e: Event) => void }> = [];
 
+  let focusedByKeyboard = false;
+  let precededByKeydownEvent = false;
+
   let inputElement: HTMLInputElement | HTMLTextAreaElement;
   onMount(() => autofocus && inputElement.focus());
 
-  function toNumber(string: string) {
-    if (string === '') {
-      return NaN;
-    }
-    return parseFloat(string);
-  }
-
-  function toString(value: string | number | null) {
-    if (value == null) {
-      return '';
-    }
-    return value.toString();
-  }
-
-  function handleInput(
-    event: Event & {
-      currentTarget: EventTarget & (HTMLInputElement | HTMLTextAreaElement);
-    }
-  ) {
-    value =
-      $$restProps.type === 'number'
-        ? toNumber(event.currentTarget.value)
-        : event.currentTarget.value;
-    dispatch('input', { value, nativeEvent: event });
-  }
-
   const dispatch = createEventDispatcher<{
-    input: { value: string | number; nativeEvent: Event };
-    change: { value: string | number; nativeEvent: Event };
+    input: { value: string; nativeEvent: Event };
+    change: { value: string; nativeEvent: Event };
     focus: { nativeEvent: FocusEvent };
     keydown: { nativeEvent: KeyboardEvent };
     blur: { nativeEvent: FocusEvent };
   }>();
 </script>
 
-<div
-  class={classes('text-field', _class)}
-  class:outline
-  class:with-item={withItem}
-  class:left={withItem && !itemRight}
-  class:right={withItem && itemRight}
-  class:no-spinner={noSpinner}
+<svelte:body
+  on:keydown={e => {
+    precededByKeydownEvent = e.key === 'Tab';
+  }}
+/>
+
+<label for={id}>{label}</label>
+<label
+  role="presentation"
+  class={classes('text-field', variant, _class)}
+  class:prominent-focus={focusedByKeyboard}
 >
-  {#if multiline}
-    <textarea
-      {id}
-      {value}
-      class={classes(inputClass)}
-      bind:this={inputElement}
-      on:input={handleInput}
-      on:change={e => dispatch('change', { value, nativeEvent: e })}
-      on:focus={e => dispatch('focus', { nativeEvent: e })}
-      on:keydown={e => dispatch('keydown', { nativeEvent: e })}
-      on:blur={e => dispatch('blur', { nativeEvent: e })}
-      use:eventsAction={events}
-      {...$$restProps}
-    />
-  {:else}
-    <input
-      {id}
-      value={toString(value)}
-      class={classes(inputClass)}
-      bind:this={inputElement}
-      on:input={handleInput}
-      on:change={e => dispatch('change', { value, nativeEvent: e })}
-      on:focus={e => dispatch('focus', { nativeEvent: e })}
-      on:keydown={e => dispatch('keydown', { nativeEvent: e })}
-      on:blur={e => dispatch('blur', { nativeEvent: e })}
-      use:eventsAction={events}
-      {...$$restProps}
-    />
-    {#if outline && label != null}
-      <label for={id} class={classes(labelClass)}>{label}</label>
-    {/if}
+  <slot name="before" />
 
-    {#if withItem}
-      <slot />
-    {/if}
+  <svelte:element
+    this={multiline ? 'textarea' : 'input'}
+    {id}
+    on:focus|self={() => {
+      if (precededByKeydownEvent) {
+        focusedByKeyboard = true;
+      }
+    }}
+    on:blur={() => {
+      precededByKeydownEvent = false;
+      focusedByKeyboard = false;
+    }}
+    on:mousedown={() => (precededByKeydownEvent = false)}
+  />
 
-    {#if error}
-      <span class={classes('error', errorClass)}>
-        <slot name="error">{error}</slot>
-      </span>
-    {/if}
-  {/if}
-</div>
+  <slot name="after" />
+</label>
 
-<style src="./text-field.scss">
+<!-- <div
+    class={classes('text-field', _class)}
+    class:outline
+    class:with-item={withItem}
+    class:left={withItem && !itemRight}
+    class:right={withItem && itemRight}
+    class:no-spinner={noSpinner}
+  >
+    {#if multiline}
+      <textarea
+        {id}
+        {value}
+        class={classes(inputClass)}
+        bind:this={inputElement}
+        on:input={handleInput}
+        on:change={e => dispatch('change', { value, nativeEvent: e })}
+        on:focus={e => dispatch('focus', { nativeEvent: e })}
+        on:keydown={e => dispatch('keydown', { nativeEvent: e })}
+        on:blur={e => dispatch('blur', { nativeEvent: e })}
+        use:eventsAction={events}
+        {...$$restProps}
+      />
+    {:else}
+      <input
+        {id}
+        value={toString(value)}
+        class={classes(inputClass)}
+        bind:this={inputElement}
+        on:input={handleInput}
+        on:change={e => dispatch('change', { value, nativeEvent: e })}
+        on:focus={e => dispatch('focus', { nativeEvent: e })}
+        on:keydown={e => dispatch('keydown', { nativeEvent: e })}
+        on:blur={e => dispatch('blur', { nativeEvent: e })}
+        use:eventsAction={events}
+        {...$$restProps}
+      />
+      {#if outline && label != null}
+        <label for={id} class={classes(labelClass)}>{label}</label>
+      {/if}
+
+      {#if withItem}
+        <slot />
+      {/if}
+
+      {#if error}
+        <span class={classes('error', errorClass)}>
+          <slot name="error">{error}</slot>
+        </span>
+      {/if}
+    {/if}
+  </div>
+-->
+<style lang="scss">
+  .text-field {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    border: 0 solid rgba(var(--a-on-surface-rgb), 0.5);
+    padding: 0 12px;
+    height: 48px;
+    gap: 6px;
+    transition: border-width 75ms;
+    cursor: text;
+
+    &.underlined {
+      border-bottom-width: 1px;
+      border-top-left-radius: var(--a-text-field-radius);
+      border-top-right-radius: var(--a-text-field-radius);
+      background: rgba(var(--a-on-surface-rgb), 0.05);
+
+      &:hover {
+        background: rgba(var(--a-on-surface-rgb), 0.07);
+      }
+
+      &:focus-within {
+        background: hsla(var(--a-primary-hsl), 0.05);
+        border-bottom-width: 2px;
+        border-bottom-color: var(--a-primary);
+
+        &:hover {
+          background: hsla(var(--a-primary-hsl), 0.07);
+        }
+      }
+    }
+
+    &.prominent-focus {
+      outline: 2px solid var(--a-secondary);
+      outline-offset: 2px;
+    }
+
+    input {
+      border: 0;
+      background: none;
+      flex: 1;
+      outline: 0;
+      height: 100%;
+    }
+  }
 </style>
