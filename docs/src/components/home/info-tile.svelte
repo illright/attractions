62b656f0066df2b1e33774f6caab1f0a287e932f
illright <leva181777@gmail.com>
2024-03-01 18:26:44 +0100

    Improve a11y of the docs

--- docs/src/components/home/info-tile.svelte
+++ docs/src/components/home/info-tile.svelte
@@ -8,7 +8,17 @@
   export let href;
 </script>
 
-<div class="info-tile" on:click={() => goto(href)}>
+<div
+  class="info-tile"
+  role="button"
+  on:click={() => goto(href)}
+  tabindex="0"
+  on:keydown={e => {
+    if (e.key == ' ' || e.key == 'Enter') {
+      goto(href);
+    }
+  }}
+>
   <Card>
     <div class="icon">
       <svelte:component this={icon} size="24" />
